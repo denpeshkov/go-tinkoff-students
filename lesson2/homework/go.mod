ec2243ca3edbb799c5b914dac8a91f00ce1bd947
Denis Peshkov <denpeshkov98@gmail.com>
2024-03-13 22:12:00 +0400

    add lesson2/tagcloud

--- lesson2/homework/go.mod
+++ lesson2/homework/go.mod
@@ -1,6 +1,6 @@
 module lecture02_homework
 
-go 1.19
+go 1.23
 
 require github.com/stretchr/testify v1.8.2
 
--- lesson2/homework/tagcloud/specification.go
+++ lesson2/homework/tagcloud/specification.go
@@ -1,8 +1,13 @@
 package tagcloud
 
+import (
+	"cmp"
+	"slices"
+)
+
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
-	// TODO: add fields if necessary
+	m map[string]int
 }
 
 // TagStat represents statistics regarding single tag
@@ -12,26 +17,30 @@ type TagStat struct {
 }
 
 // New should create a valid TagCloud instance
-// TODO: You decide whether this function should return a pointer or a value
-func New() TagCloud {
-	// TODO: Implement this
-	return TagCloud{}
+func New() *TagCloud {
+	return &TagCloud{
+		m: map[string]int{},
+	}
 }
 
 // AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
 // thread-safety is not needed
-// TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) AddTag(tag string) {
-	// TODO: Implement this
+func (t *TagCloud) AddTag(tag string) {
+	t.m[tag]++
 }
 
 // TopN should return top N most frequent tags ordered in descending order by occurrence count
 // if there are multiple tags with the same occurrence count then the order is defined by implementation
 // if n is greater that TagCloud size then all elements should be returned
 // thread-safety is not needed
-// there are no restrictions on time complexity
-// TODO: You decide whether receiver should be a pointer or a value
-func (TagCloud) TopN(n int) []TagStat {
+func (t *TagCloud) TopN(n int) []TagStat {
 	// TODO: Implement this
-	return nil
+	ts := make([]TagStat, len(t.m))
+	i := 0
+	for k, v := range t.m {
+		ts[i] = TagStat{k, v}
+		i++
+	}
+	slices.SortFunc(ts, func(a, b TagStat) int { return cmp.Compare(b.OccurrenceCount, a.OccurrenceCount) })
+	return ts[:min(len(t.m), n)]
 }
